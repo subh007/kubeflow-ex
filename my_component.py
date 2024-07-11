6caf71669da2369a17450f1692a912defebde885
subhash kumar singh <subh.singh007@gmail.com>
2024-07-11 21:43:45 +0530

    example for light weight component

--- my_component.py
+++ my_component.py
@@ -3,4 +3,9 @@ from kfp import dsl
 @dsl.component(base_image='python:3.7',
                target_image='gcr.io/my-project/my-component:v1')
 def add_number(a: int, b: int) -> int:
-    sum(a,b)
+    return a + b
+
+# light weight component
+@dsl.component
+def sub_number(a: int, b: int) -> int:
+    return a - b
