8332969387a0c4b187316fee65a6424e2952825c
subhash kumar singh <subh.singh007@gmail.com>
2024-07-11 22:24:26 +0530

    example for running pipeline

--- pipeline.py
+++ pipeline.py
@@ -6,3 +6,7 @@ def hello_pipeline():
     say_hello()
 
 compiler.Compiler().compile(hello_pipeline, 'pipeline.yaml')
+
+from kfp.client import Client
+client = Client(host='http://localhost:8080')
+client.create_run_from_pipeline_package('pipeline.yaml', arguments={})
