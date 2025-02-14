7c8bbf1a5c1e336025ba4a3f33fff8b1f6511971
subhash kumar singh <subh.singh007@gmail.com>
2025-02-14 12:06:17 +0530

    added few more component

---
+++ src/nn_component.py
@@ -0,0 +1,34 @@
+from kfp import dsl
+from kfp.dsl import Dataset, Output
+from kfp import local
+
+@dsl.component(packages_to_install=['tensorflow==2.18.0', 'pandas'],
+               base_image='python:3.10', target_image='ghcr.io/subh007/pd_test:v1',
+               pip_index_urls='http://localhost/simple/')
+def prepare_data(boston_data: Output[Dataset]):
+    import tensorflow as tf
+    import numpy as np
+    import pandas as pd
+    (train_x, train_y), (test_X, test_Y) = tf.keras.datasets.boston_housing.load_data(test_split=0)
+    # Convert to DataFrame
+    column_names = [
+        "CRIM", "ZN", "INDUS", "CHAS", "NOX", "RM", "AGE", "DIS", "RAD", "TAX", "PTRATIO", "B", "LSTAT", "MEDV"
+    ]
+    df = pd.DataFrame(np.column_stack((train_x, train_y)), columns=column_names)
+
+    # Save to CSV file
+    # df.to_csv(boston_data.path, index=False)
+    with open(boston_data.path, "wb") as f:
+        df.to_csv(f, index=False)
+
+    print(f"Boston Housing dataset saved to {boston_data.path}")
+
+
+
+# import kfp
+# kfp.local.init(runner=local.DockerRunner())
+
+# prepare_data()
+    
+from kfp import compiler
+compiler.Compiler().compile(prepare_data, 'component.yaml')
---
+++ src/nn_components/prepare_data.py
@@ -0,0 +1,33 @@
+from kfp import dsl
+from kfp.dsl import Dataset, Output
+from kfp import local
+
+@dsl.component(packages_to_install=['tensorflow==2.18.0', 'pandas'],
+               base_image='python:3.10', target_image='ghcr.io/subh007/pd_test:v1',
+               pip_index_urls='http://localhost/simple/')
+def prepare_data(data: Output[Dataset]):
+    import tensorflow as tf
+    import numpy as np
+    import pandas as pd
+    (train_x, train_y), (test_X, test_Y) = tf.keras.datasets.boston_housing.load_data(test_split=0)
+    # Convert to DataFrame
+    column_names = [
+        "CRIM", "ZN", "INDUS", "CHAS", "NOX", "RM", "AGE", "DIS", "RAD", "TAX", "PTRATIO", "B", "LSTAT", "MEDV"
+    ]
+    df = pd.DataFrame(np.column_stack((train_x, train_y)), columns=column_names)
+
+    # Save to CSV file
+    output_path = boston_data.path + ".csv"  # Ensuring correct file format
+    df.to_csv(output_path, index=False)
+
+    print(f"Boston Housing dataset saved to {output_path}")
+
+
+
+# import kfp
+# kfp.local.init(runner=local.DockerRunner())
+
+# prepare_data()
+    
+from kfp import compiler
+compiler.Compiler().compile(prepare_data, 'component.yaml')
